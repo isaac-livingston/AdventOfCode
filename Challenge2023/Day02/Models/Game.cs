376c8e37886b87c9bb964e08aaefe48777ef7ed8
Isaac Livingston <isaac@logical-insight.net>
2023-12-02 11:58:02 -0500

    problem 1 solution: 3099

---
+++ Challenge2023/Day02/Models/Game.cs
@@ -0,0 +1,8 @@
+﻿namespace Challenge2023.Day02.Models
+{
+    internal class Game
+    {
+        public int Id { get; set; }
+        public List<GameDataSet> Results { get; set; } = [];
+    }
+}
---
+++ Challenge2023/Day02/Models/GameDataSet.cs
@@ -0,0 +1,9 @@
+﻿namespace Challenge2023.Day02.Models
+{
+    internal class GameDataSet
+    {
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+    }
+}
