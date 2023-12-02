0ef02e14f561e804ede9e8ac1b8e0d530c79e2c2
Isaac Livingston <isaac@logical-insight.net>
2023-12-02 12:12:45 -0500

    tidy up

--- Challenge2023/Program.cs
+++ Challenge2023/Program.cs
@@ -1,9 +1,9 @@
 ﻿
 
-//(new AdventOfCode.Day01.Problem2()).RunSolution();
+//new AdventOfCode.Day01.Problem2().RunSolution();
 
-(new Challenge2023.Day02.Problem1()).RunSolution();
-(new Challenge2023.Day02.Problem2()).RunSolution();
+//new Challenge2023.Day02.Problem1().RunSolution();
+new Challenge2023.Day02.Problem2().RunSolution();
 
 Console.WriteLine();
 Console.WriteLine("fin.");
