1b2ae573fe117d2b1049c5324539398076941f9b
Isaac Livingston <isaac@logical-insight.net>
2023-12-04 06:42:37 -0500

    a few minor tweaks to help efficiency.

--- Challenge2023/Day04/Day04Base.cs
+++ Challenge2023/Day04/Day04Base.cs
@@ -14,41 +14,39 @@ namespace Challenge2023.Day04
         {
             var winners = cards.Where(x => x.MatchedNumbers.Length != 0).ToList();
 
-            var expandedSet = new List<ScratchCard>();
+            var cardTable = cards.ToDictionary(x => x.CardId, x => x);
 
             foreach (var winner in winners)
             {
                 for(var i = 1; i < winner.MatchedNumbers.Length + 1; i++)
                 {
-                    var cardToAdd = cards.First(x => x.CardId == winner.CardId + i);
+                    var cardToAdd = cardTable[winner.CardId + i];
 
-                    expandedSet.Add(cardToAdd);
+                    cards.Add(cardToAdd);
 
                     if (cardToAdd.Winnings > 0)
                     {
-                        expandedSet.AddRange(ExpandCards(cardToAdd, cards, null));
+                        cards.AddRange(ExpandCards(cardToAdd, cardTable, null));
                     }
                 }
             }
 
-            cards.AddRange(expandedSet);
-
             return cards;
         }
 
-        private List<ScratchCard> ExpandCards(ScratchCard card, List<ScratchCard> cards, List<ScratchCard>? expandedSet)
+        private List<ScratchCard> ExpandCards(ScratchCard card, Dictionary<int, ScratchCard> cardTable, List<ScratchCard>? expandedSet)
         {
             expandedSet ??= [];
 
             for (var i = 1; i < card.MatchedNumbers.Length + 1; i++)
             {
-                var cardToAdd = cards.First(x => x.CardId == card.CardId + i);
+                var cardToAdd = cardTable[card.CardId + i];
 
                 expandedSet.Add(cardToAdd);
 
                 if (cardToAdd.Winnings > 0)
                 {
-                    ExpandCards(cardToAdd, cards, expandedSet);
+                    ExpandCards(cardToAdd, cardTable, expandedSet);
                 }
             }
 
--- Challenge2023/Day04/Models/ScratchCard.cs
+++ Challenge2023/Day04/Models/ScratchCard.cs
@@ -18,13 +18,13 @@
             WinningNumbers = numbers[0];
             GivenNumbers = numbers[1];
             MatchedNumbers = WinningNumbers.Intersect(GivenNumbers).ToArray();
+            Winnings = (long)Math.Pow(2, MatchedNumbers.Length - 1);
         }
 
         public int CardId { get; }
         public int[] WinningNumbers { get; }
         public int[] GivenNumbers { get; }
         public int[] MatchedNumbers { get; }
-
-        public long Winnings => (long)Math.Pow(2, MatchedNumbers.Length - 1);
+        public long Winnings { get; } 
     }
 }
