130e9b4f402c98e6b8002887a0ac260753721f4f
Isaac Livingston <isaac@logical-insight.net>
2023-12-07 20:19:11 -0500

    probelm 1 solve

--- Challenge2023/Day07/Day07Base.cs
+++ Challenge2023/Day07/Day07Base.cs
@@ -7,26 +7,90 @@ namespace Challenge2023.Day07
     {
         public Day07Base() 
         {
-            var cards = new Dictionary<string, int>
-            {
-                ["2"] = 1 << 2,
-                ["3"] = 1 << 3,
-                ["4"] = 1 << 4,
-                ["5"] = 1 << 5,
-                ["6"] = 1 << 6,
-                ["7"] = 1 << 7,
-                ["8"] = 1 << 8,
-                ["9"] = 1 << 9,
-                ["T"] = 1 << 10,
-                ["J"] = 1 << 11,
-                ["Q"] = 1 << 12,
-                ["K"] = 1 << 13,
-                ["A"] = 1 << 14
-            };
-
-            Cards = cards.ToFrozenDictionary();
+            DefineCards();
         }
 
-        protected FrozenDictionary<string, int> Cards { get; set; }
+        protected abstract void DefineCards();
+
+        protected FrozenDictionary<char, int> Cards { get; set; }
+
+        protected Dictionary<string, (long bet, long scale, long[] power)> Hands { get; private set; } = [];
+
+        protected void LoadHands(string[] inputs)
+        {
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                var data = inputs[i].Split(' ', SPLIT_OPTS);
+                var (scale, power) = MakeHand(data[0]);
+
+                Hands[data[0]] = (long.Parse(data[1]), scale, power);
+            }
+
+            Hands = Hands.OrderBy(x => x.Value.scale)
+                         .ThenBy(x => x.Value.power[0])
+                         .ThenBy(x => x.Value.power[1])
+                         .ThenBy(x => x.Value.power[2])
+                         .ThenBy(x => x.Value.power[3])
+                         .ThenBy(x => x.Value.power[4])
+                         .ToDictionary();
+        }
+
+        private (long scale, long[] power) MakeHand(string handInput)
+        {
+            var hand = handInput.ToCharArray();
+            var power = new long[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                power[i] = Cards[hand[i]];
+            }
+
+            return (GetHandScale(handInput), power);
+        }
+
+        static int GetHandScale(string handInput)
+        {
+            var cardCounts = handInput.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+            
+            if (cardCounts.ContainsValue(5))
+            {
+                return 6;
+            }
+            else if(cardCounts.ContainsValue(4))
+            {
+                return 5;
+            }
+            else if (cardCounts.ContainsValue(3) && cardCounts.ContainsValue(2))
+            {
+                return 4;
+            }
+            else if (cardCounts.ContainsValue(3))
+            {
+                return 3;
+            }
+            else if (cardCounts.ContainsValue(2))
+            {
+                var xx = GroupByCardCount(cardCounts);
+
+                if (xx.ContainsValue(2))
+                {
+                    return 2;
+                }
+
+                return 1;
+            }
+
+            return 0;
+        }
+
+        static Dictionary<int, int> GroupByCardCount(Dictionary<char, int> cardCounts)
+        {
+            return cardCounts
+                .GroupBy(entry => entry.Value)
+                .ToDictionary(
+                    group => group.Key,
+                    group => group.Select(entry => entry.Key).Count()
+                );
+        }
     }
 }
--- Challenge2023/Day07/Problem2.cs
+++ Challenge2023/Day07/Problem2.cs
@@ -1,21 +1,92 @@
 ﻿using Challenge2023.Common;
+using System.Collections.Frozen;
+using System.Text;
 
 namespace Challenge2023.Day07
 {
     internal class Problem2 : Day07Base
     {
+        protected override void DefineCards()
+        {
+            var cards = new Dictionary<char, int>
+            {
+                ['2'] = 1 << 2,
+                ['3'] = 1 << 3,
+                ['4'] = 1 << 4,
+                ['5'] = 1 << 5,
+                ['6'] = 1 << 6,
+                ['7'] = 1 << 7,
+                ['8'] = 1 << 8,
+                ['9'] = 1 << 9,
+                ['T'] = 1 << 10,
+                ['J'] = 1 << 0,
+                ['Q'] = 1 << 12,
+                ['K'] = 1 << 13,
+                ['A'] = 1 << 14
+            };
+
+            Cards = cards.ToFrozenDictionary();
+        }
+
         public override void RunSolution()
         {
             stopwatch.Start();
             var inputs = GetInputs(folder: "day07");
 
-            //InitializeInput(inputs);
+            //inputs = [
+            //    "32T3K 765",
+            //    "T55J5 684",
+            //    "KK677 28",
+            //    "KTJJT 220",
+            //    "QQQJA 483",
+            //    "QQQAA 483",
+            //];
 
-            //var solution = GetSolutions();
+            //inputs = [
+            //    "2345A 1",
+            //    "Q2KJJ 13",
+            //    "Q2Q2Q 19",
+            //    "T3T3J 17",
+            //    "T3Q33 11",
+            //    "2345J 3",
+            //    "J345A 2",
+            //    "32T3K 5",
+            //    "T55J5 29",
+            //    "KK677 7",
+            //    "KTJJT 34",
+            //    "QQQJA 31",
+            //    "JJJJJ 37",
+            //    "JAAAA 43",
+            //    "AAAAJ 59",
+            //    "AAAAA 61",
+            //    "2AAAA 23",
+            //    "2JJJJ 53",
+            //    "JJJJ2 41",
+            //];
+
+            LoadHands(inputs);
+            var sb = new StringBuilder();
+            foreach (var key in Hands.Keys)
+            {
+                var val = $"{key} {Hands[key].bet}:\t{Hands[key].scale}\t{Hands[key].power[0]}\t{Hands[key].power[1]}\t{Hands[key].power[2]}\t{Hands[key].power[3]}\t{Hands[key].power[4]}";
+                sb.AppendLine(val);
+                Console.WriteLine(val);
+            }
+
+            File.WriteAllText("output.csv", sb.ToString());
+
+            var solution = 0L;
+            var iter = 1L;
+
+            foreach (var key in Hands.Keys)
+            {
+                solution += Hands[key].bet * iter;
+                iter++;
+            }
 
             stopwatch.Stop();
 
-            //ConsoleTools.PrintSolutionMessage(solution);
+            ConsoleTools.PrintSolutionMessage(solution);
             ConsoleTools.PrintDurationMessage(stopwatch.ElapsedMilliseconds);
         }
     }
