f5e659b96dc72cef447ed1609a5a3ef6950a0321
Isaac Livingston <isaac@logical-insight.net>
2023-12-08 09:25:19 -0500

    Da 08 Setup

---
+++ Challenge2023/Day08/Problem1.cs
@@ -0,0 +1,21 @@
+﻿using Challenge2023.Common;
+
+namespace Challenge2023.Day08
+{
+    internal class Problem1 : Day08Base
+    {
+        public override void RunSolution()
+        {
+            var inputs = GetInputs(folder: "day08");
+
+            stopwatch.Start();
+
+            //fun things
+
+            stopwatch.Stop();
+
+
+            ConsoleTools.PrintDurationMessage(stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ Challenge2023/Day08/Problem2.cs
@@ -0,0 +1,21 @@
+﻿using Challenge2023.Common;
+
+namespace Challenge2023.Day08
+{
+    internal class Problem2 : Day08Base
+    {
+        public override void RunSolution()
+        {
+            var inputs = GetInputs(folder: "day08");
+            
+            stopwatch.Start();
+
+            //fun things
+
+            stopwatch.Stop();
+
+            
+            ConsoleTools.PrintDurationMessage(stopwatch.ElapsedMilliseconds);
+        }
+    }
+}
