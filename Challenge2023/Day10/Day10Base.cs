053de43e5c25a61194649b0b241ea9fa0b666bbe
Isaac Livingston <isaac@logical-insight.net>
2023-12-10 14:38:33 -0500

    clean up

--- Challenge2023/Day10/Day10Base.cs
+++ Challenge2023/Day10/Day10Base.cs
@@ -1,10 +1,5 @@
 ﻿using Challenge2023.Common;
-using System;
 using System.Collections.Frozen;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Challenge2023.Day10
 {
