2902b7e16db7877edf8768ee31e7ba9d6ee9f6ea
Isaac Livingston <isaac@logical-insight.net>
2023-12-12 10:58:52 -0500

    thoughts on D10P2

--- Challenge2023/Day10/Day10Base.cs
+++ Challenge2023/Day10/Day10Base.cs
@@ -1,5 +1,6 @@
 ﻿using Challenge2023.Common;
 using System.Collections.Frozen;
+using System.Text;
 
 namespace Challenge2023.Day10
 {
@@ -28,8 +29,35 @@ namespace Challenge2023.Day10
 
         protected int[][] Grid = [];
 
+        private string[] GetExpandedInputs(string[] inputs)
+        {
+            var expandedInputs = new List<string>();
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                var input = inputs[i];
+                var sb = new StringBuilder();
+                foreach (char ch in input)
+                {
+                    sb.Append(ch);
+                    sb.Append('-');
+                    sb.Append('-');
+                }
+
+                var expandedInput = sb.ToString();
+
+                expandedInputs.Add(expandedInput);
+                expandedInputs.Add("".PadLeft(expandedInput.Length, '|'));
+                expandedInputs.Add("".PadLeft(expandedInput.Length, '|'));
+            }
+
+            return [.. expandedInputs];
+        }
+
         protected void LoadData(string[] inputs)
         {
+            //inputs = expandForProblem2 ? GetExpandedInputs(inputs) : inputs;
+
             var rows = inputs.Length;
 
             Grid = new int[rows][];
@@ -80,14 +108,16 @@ namespace Challenge2023.Day10
             _ => throw new InvalidOperationException("Invalid direction")
         };
 
-        protected Dictionary<int[], long> TraverseNetwork(int comingInFrom)
+        protected static string RCNotation(int r, int c) => $"r{r}c{c}";
+
+        protected Dictionary<string, long> TraverseNetwork(int comingInFrom)
         {
             var row = StartingCell[0];
             var col = StartingCell[1];
 
             var steps = 0L;
-            var record = new Dictionary<int[], long> { 
-                { [row, col], 0 } 
+            var record = new Dictionary<string, long> { 
+                { RCNotation(row, col), 0 } 
             };
             while (true)
             {
@@ -98,7 +128,7 @@ namespace Challenge2023.Day10
 
                 comingInFrom = ComingFrom(move);
 
-                record[[row, col]] = steps;
+                record[RCNotation(row, col)] = steps;
 
                 if (row == StartingCell[0] && col == StartingCell[1])
                 {
@@ -109,5 +139,22 @@ namespace Challenge2023.Day10
 
             return record;
         }
+
+        //protected static bool IsPointInsidePolygon(double[] x, double[] y, double testX, double testY)
+        //{
+        //    int n = x.Length;
+        //    bool inside = false;
+
+        //    for (int i = 0, j = n - 1; i < n; j = i++)
+        //    {
+        //        if (((y[i] > testY) != (y[j] > testY)) &&
+        //            (testX < (x[j] - x[i]) * (testY - y[i]) / (y[j] - y[i]) + x[i]))
+        //        {
+        //            inside = !inside;
+        //        }
+        //    }
+
+        //    return inside;
+        //}
     }
 }
--- Challenge2023/Day10/Problem2.cs
+++ Challenge2023/Day10/Problem2.cs
@@ -16,13 +16,18 @@ namespace Challenge2023.Day10
             //    "....."
             //];
 
-            //inputs = [
-            //    "7-F7-",
-            //    ".FJ|7",
-            //    "SJLL7",
-            //    "|F--J",
-            //    "LJ.LJ"
-            //];
+            inputs = [
+                "FF7FSF7F7F7F7F7F---7",
+                "L|LJ||||||||||||F--J",
+                "FL-7LJLJ||||||LJL-77",
+                "F--JF--7||LJLJ7F7FJ-",
+                "L---JF-JLJ.||-FJLJJ7",
+                "|F|F-JF---7F7-L7L|7|",
+                "|FFJF7L7F-JF7|JL---7",
+                "7-L-JL7||F7|L7F-7F7|",
+                "L.L7LFJ|||||FJL7||LJ",
+                "L7JLJL-JLJLJL--JLJ.L"
+            ];
 
             stopwatch.Start();
 
@@ -30,7 +35,24 @@ namespace Challenge2023.Day10
 
             var runFromEast = TraverseNetwork(comingInFrom: EAST);
 
-            var solution = runFromEast.Values.ElementAt((runFromEast.Count / 2) + 1);
+            var y = runFromEast.Keys.Select(r => (double)r[0]).ToArray();
+            var x = runFromEast.Keys.Select(c => (double)c[1]).ToArray();
+
+            var insideCount = 0L;
+
+            for (int r = 0; r < Grid.Length; r++)
+            {
+                for (int c = 0; c < Grid[r].Length; c++)
+                {
+                    if (!runFromEast.ContainsKey(RCNotation(r, c)))
+                    {
+                        insideCount++;
+                    }
+                }
+            }
+
+            //var solution = runFromEast.Values.ElementAt((runFromEast.Count / 2) + 1);
+            var solution = insideCount;// runFromEast.Values.ElementAt((runFromEast.Count / 2) + 1);
 
             stopwatch.Stop();
 
