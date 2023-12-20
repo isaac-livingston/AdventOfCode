6e70d8f8a627339791ae579b5e6f9ecf850c1bd8
Isaac Livingston <isaac@logical-insight.net>
2023-12-20 17:06:38 -0500

    adding day 19 folder

---
+++ Challenge2023/Day19/DayBase.cs
@@ -0,0 +1,8 @@
+﻿using Challenge2023.Common;
+
+namespace Challenge2023.Day19;
+
+internal abstract class DayBase : ProblemBase
+{
+    protected const string DAY_FOLDER = "Day19";
+}
---
+++ Challenge2023/Day19/Problem1.cs
@@ -0,0 +1,21 @@
+﻿using Challenge2023.Common;
+
+namespace Challenge2023.Day19;
+
+internal class Problem1 : DayBase
+{
+    public override void RunSolution()
+    {
+        var inputs = GetInputs(folder: DAY_FOLDER, useTest: true);
+
+        stopwatch.Start();
+
+        var solution = 0L;
+
+        stopwatch.Stop();
+
+        ConsoleTools.PrintSolutionMessage($"{solution}");
+        ConsoleTools.PrintDurationMessage(stopwatch.ElapsedMilliseconds);
+    }
+}
+
