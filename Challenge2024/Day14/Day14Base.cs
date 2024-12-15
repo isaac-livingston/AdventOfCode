6e5715d576e5fa3dc8e1a1e2fa47c47ee4b96a87
Isaac Livingston <isaac@logical-insight.net>
2024-12-14 22:52:24 -0500

    day 14 setup

---
+++ Challenge2024/Day14/Day14Base.cs
@@ -0,0 +1,9 @@
+﻿namespace Challenge2024.Day14;
+
+internal class Day14Base : ProblemBase
+{
+    public void ParseInputs(string[] inputs, long offset = 0)
+    {
+        
+    }
+}
---
+++ Challenge2024/Day14/Problem1.cs
@@ -0,0 +1,26 @@
+﻿namespace Challenge2024.Day14;
+
+internal class Problem1 : Day14Base
+{
+    public override void RunSolution()
+    {
+        var inputs = GetInputs(folder: "day14", false);
+        ParseInputs(inputs);
+
+        var spend = 0D;
+
+        foreach(var machine in Machines)
+        {
+            Console.WriteLine(machine);
+
+            var winnable = machine.PrizeWinnable();
+
+            if (winnable)
+            {
+                spend += machine.TotalCost;
+            }
+        }
+
+        Console.WriteLine($"Total spend: {spend}");
+    }
+}
