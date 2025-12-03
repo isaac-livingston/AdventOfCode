a69ced2a1b14ad33a7cb8bdb53f7828d254ed358
Isaac Livingston <isaac@logical-insight.net>
2025-12-03 09:22:14 -0500

    turn off test for 03p2

--- Challenge2025/Day03/Problem2.cs
+++ Challenge2025/Day03/Problem2.cs
@@ -4,7 +4,7 @@ internal class Problem2 : DayBase
 {
     public override void RunSolution()
     {
-        var inputs = GetInputs(folder: "day03", useTest: true);
+        var inputs = GetInputs(folder: "day03", useTest: false);
         ParseInputs(inputs, greatScott: true);
 
         var joltage = PowerBankJoltages.Sum();
